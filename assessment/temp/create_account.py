16dafbb65d5a6b9b024ba19dccd95c490d331293
Nicholas Piet <nicholas.piet33@gmail.com>
2024-09-02 20:18:55 +1000

    new file:   accounts.txt 	new file:   assessment/temp/accounts.txt 	modified:   assessment/temp/create_account.py

--- assessment/temp/create_account.py
+++ assessment/temp/create_account.py
@@ -3,3 +3,9 @@
     # if account exists in the file, display an error
 # end up here if the account is available
 # prompt for password OR generate a password and display it.
+
+print("Registration function")
+with open("accounts.txt", 'r') as accounts:
+    for lines in accounts:
+        username, password = accounts.split(",").strip()
+        print(username)
