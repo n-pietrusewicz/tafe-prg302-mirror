cadb32101382fb69fc66bb6b2bbdd0db41391408
Nicholas Piet <nicholas.piet33@gmail.com>
2024-09-04 20:47:57 +1000

    modified:   assessment/passwd_module.py

--- assessment/passwd_module.py
+++ assessment/passwd_module.py
@@ -1,17 +1,19 @@
 # password module
 # generates a random password with error handling capabilties.
 
-import secrets
-import string
-CHARS = string.ascii_letters + string.digits + string.punctuation
+# import secrets
+# import string
+# CHARS = string.ascii_letters + string.digits + string.punctuation
 
-def password_gen():
-    try: 
-        user_choice = int(input("Enter a password length choice (8-512 characters): "))
-        if 8 <= user_choice <= 512:
-            password = ''.join(secrets.choice(CHARS) for i in range(user_choice))
-            print(password)
-        else:
-            print("Sorry! Length must be between 8 and 512.")
-    except ValueError:
-        print("Error: Invalid value")
+# def password_gen():
+#     try: 
+#         user_choice = int(input("Enter a password length choice (8-512 characters): "))
+#         if 8 <= user_choice <= 512:
+#             password = ''.join(secrets.choice(CHARS) for i in range(user_choice))
+#             print(password)
+#         else:
+#             print("Sorry! Length must be between 8 and 512.")
+#     except ValueError:
+#         print("Error: Invalid value")
+
+
